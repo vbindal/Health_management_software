b820f7997e4ef4c178d89ab972ae8b4cf0d57e96
Vishal Bindal <77441928+vbindal@users.noreply.github.com>
2021-11-30 16:16:05 +0530

    Update Health management system.py

--- Health management system.py
+++ Health management system.py
@@ -12,37 +12,37 @@ def log(k):
         c= int(input("Enter 1 for exercise and 2 for food:"))
         if c==1:
             value= input("Type here:")
-            with open("harry-ex.txt","a") as op:
-                op.write(str([getdate()])+":"+value+"\n")
+            with open("harry-ex.txt","a") as txt:
+                Txt.write(str([getdate()])+":"+value+"\n")
                 print("Successfully written")
         if c==2:
             value= (input("Type here:"))
-            with open("harry-fd.txt","a") as op:
-                op.write(str([getdate()])+":"+value+"\n")
+            with open("harry-fd.txt","a") as txt:
+                Txt.write(str([getdate()])+":"+value+"\n")
                 print("Successfully written")
     elif k==2:
         l = int(input("Enter 1 for exercise and 2 for food:"))
         if l == 1:
             value = input("Type here:")
-            with open("hammad-ex.txt", "a") as op:
-                op.write(str([getdate()])+":"+value+"\n")
+            with open("hammad-ex.txt", "a") as txt:
+                Txt.write(str([getdate()])+":"+value+"\n")
                 print("Successfully written")
         if l == 2:
             value = input("Type here:")
-            with open("hammad-fd.txt", "a") as op:
-                op.write(str([getdate()])+":"+value+"\n")
+            with open("hammad-fd.txt", "a") as txt:
+                txt.write(str([getdate()])+":"+value+"\n")
                 print("Successfully written")
     elif k==3:
         m = int(input("Enter 1 for exercise and 2 for food:"))
         if m == 1:
             value = input("Type here:")
-            with open("Rohan-ex.txt", "a") as op:
-                op.write(str([getdate()])+":"+value+"\n")
+            with open("Rohan-ex.txt", "a") as txt:
+                txt.write(str([getdate()])+":"+value+"\n")
                 print("Successfully written")
         if m == 2:
             value = input("Type here:")
-            with open("Rohan-fd.txt", "a") as op:
-                op.write(str([getdate()])+":"+value+"\n")
+            with open("Rohan-fd.txt", "a") as txt:
+                txt.write(str([getdate()])+":"+value+"\n")
                 print("Successfully written")
     else:
         print("please enter a valid input for harry,hammad and rohan")
@@ -50,32 +50,32 @@ def retrieve(k):
     if k==1:
         c= int(input("Enter 1 for exercise and 2 for food:"))
         if c==1:
-            with open("harry-ex.txt") as op:
+            with open("harry-ex.txt") as txt:
                 for i in op:
                     print(i,end="")
         if c==2:
-            with open("harry-fd.txt") as op:
+            with open("harry-fd.txt") as txt:
                 for i in op:
                     print(i,end="")
 
     if k==2:
         l= int(input("Enter 1 for exercise and 2 for food:"))
         if l==1:
-            with open("hammad-ex.txt") as op:
+            with open("hammad-ex.txt") as txt:
                 for i in op:
                     print(i,end="")
         if l==2:
-            with open("hammad-fd.txt") as op:
+            with open("hammad-fd.txt") as txt:
                 for i in op:
                     print(i,end="")
     if k==3:
         m= int(input("Enter 1 for exercise and 2 for food:"))
         if m==1:
-            with open("Rohan-ex.txt") as op:
+            with open("Rohan-ex.txt") as txt:
                 for i in op:
                     print(i,end="")
         if m==2:
-            with open("Rohan-fd.txt") as op:
+            with open("Rohan-fd.txt") as txt:
                 for i in op:
                     print(i,end="")
 ab= print("[HEALTH MANAGEMENT SYSTEM]")
